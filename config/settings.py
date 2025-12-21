385b6d2ef27d949e423f0a5c7d0ea0494ff91c96
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-12-22 00:02:08 +0600

    style: formatting fix

--- config/settings.py
+++ config/settings.py
@@ -300,7 +300,9 @@ REST_FRAMEWORK = {
         "rest_framework.filters.SearchFilter",
         "rest_framework.filters.OrderingFilter",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": (
+        "rest_framework.pagination." "PageNumberPagination"
+    ),
     "PAGE_SIZE": 20,
 }
 
